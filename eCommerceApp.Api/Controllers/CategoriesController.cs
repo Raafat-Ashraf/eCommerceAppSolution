04b83f91b0b9ddc97bea2fef8f322525efb53cab
Raafat Ashraf <raafat.ashraf.17@gmail.com>
2024-12-04 15:55:52 +0200

    Add crud for categories

---
+++ eCommerceApp.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,50 @@
+using eCommerceApp.Application.Dtos.Category;
+
+namespace eCommerceApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController(ICategoryService categoryService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var products = await categoryService.GetAllAsync();
+
+        return Ok(products);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await categoryService.GetByIdAsync(id);
+
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCategory product)
+    {
+        var result = await categoryService.CreateAsync(product);
+
+        return result.IsSuccess ? Created() : result.ToProblem();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateCategory product)
+    {
+        var result = await categoryService.UpdateAsync(product);
+
+        return result.IsSuccess ? NoContent() : result.ToProblem();
+    }
+
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var result = await categoryService.DeleteAsync(id);
+
+        return result.IsSuccess ? NoContent() : result.ToProblem();
+    }
+}
